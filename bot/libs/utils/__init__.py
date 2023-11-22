fdacfc8e7286125bf51217c65f69454dfd8684b7
No767 <73260931+No767@users.noreply.github.com>
2023-11-22 15:58:47 -0800

    Add error handler

--- bot/libs/utils/__init__.py
+++ bot/libs/utils/__init__.py
@@ -3,7 +3,9 @@ from .checks import (
     is_manager as is_manager,
     is_mod as is_mod,
 )
+from .context import RoboContext as RoboContext
 from .embeds import Embed as Embed, ErrorEmbed as ErrorEmbed
+from .errors import send_error_embed as send_error_embed
 from .logger import RodhajLogger as RodhajLogger
 from .modals import RoboModal as RoboModal
 from .time import human_timedelta as human_timedelta
--- bot/libs/utils/errors.py
+++ bot/libs/utils/errors.py
@@ -1,6 +1,7 @@
 import traceback
 
 import discord
+from discord.ext import commands
 
 from .embeds import ErrorEmbed
 
@@ -18,3 +19,45 @@ def produce_error_embed(error: Exception) -> ErrorEmbed:
     embed.set_footer(text="Happened At")
     embed.timestamp = discord.utils.utcnow()
     return embed
+
+
+def create_premade_embed(title: str, description: str) -> ErrorEmbed:
+    embed = ErrorEmbed()
+    embed.timestamp = discord.utils.utcnow()
+    embed.title = title
+    embed.description = description
+    return embed
+
+
+async def send_error_embed(ctx: commands.Context, error: commands.CommandError) -> None:
+    if isinstance(error, commands.CommandInvokeError) or isinstance(
+        error, commands.HybridCommandError
+    ):
+        await ctx.send(embed=produce_error_embed(error))
+    elif isinstance(error, commands.CommandNotFound):
+        await ctx.send(
+            embed=create_premade_embed(
+                "Command not found",
+                "The command you were looking for could not be found",
+            )
+        )
+    elif isinstance(error, commands.NotOwner):
+        # Basically completely silence it making people not know what happened
+        return
+    elif isinstance(error, commands.MissingPermissions):
+        missing_perms = ", ".join(error.missing_permissions).rstrip(",")
+        await ctx.send(
+            embed=create_premade_embed(
+                "Missing Permissions",
+                f"You are missing the following permissions: {missing_perms}",
+            )
+        )
+    elif isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send(
+            embed=create_premade_embed(
+                "Missing Required Argument",
+                f"You are missing the following argument(s): {error.param.name}",
+            )
+        )
+    else:
+        await ctx.send(embed=produce_error_embed(error))
