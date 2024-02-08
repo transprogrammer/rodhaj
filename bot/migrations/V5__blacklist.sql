1d1d40f7f0d94e26f49b549bb875c4f2e204aa9a
No767 <73260931+No767@users.noreply.github.com>
2024-02-07 16:46:29 -0800

    Include blacklist migrations

--- bot/migrations/V5__blacklist.sql
+++ bot/migrations/V5__blacklist.sql
@@ -1,13 +1,18 @@
 -- Revision Version: V5
 -- Revises: V4
--- Creation Date: 2024-01-30 20:00:18.976633 UTC
+-- Creation Date: 2024-02-08 00:45:11.293618 UTC
 -- Reason: blacklist
+
 
 CREATE TABLE IF NOT EXISTS blacklist (
     id SERIAL PRIMARY KEY,
     guild_id BIGINT,
-    entity_id BIGINT UNIQUE
+    entity_id BIGINT UNIQUE,
+    expires TIMESTAMP,
+    created_at TIMESTAMP DEFAULT (now() at time zone 'utc')
 );
 
 CREATE INDEX IF NOT EXISTS blacklist_guild_id_idx ON blacklist (guild_id);
 CREATE INDEX IF NOT EXISTS blacklist_entity_id_idx ON blacklist (entity_id);
+CREATE INDEX IF NOT EXISTS blacklist_expires_idx ON blacklist (expires);
+```
