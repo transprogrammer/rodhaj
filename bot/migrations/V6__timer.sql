43220c50bb9a748b7648745de01131f28620ddbf
No767 <73260931+No767@users.noreply.github.com>
2024-02-14 22:43:45 -0800

    Add timer schema

--- bot/migrations/V6__timer.sql
+++ bot/migrations/V6__timer.sql
@@ -1,13 +1,14 @@
 -- Revision Version: V6
 -- Revises: V5
 -- Creation Date: 2024-02-09 06:46:23.126029 UTC
--- Reason: timer
+-- Reason: timer schema
 
 CREATE TABLE IF NOT EXISTS timers (
     id SERIAL PRIMARY KEY,
+    user_id BIGINT,
     expires TIMESTAMP,
     created TIMESTAMP DEFAULT (now() at time zone 'utc'),
-    event TEXT
+    event TEXT,
 );
 
 CREATE INDEX IF NOT EXISTS timers_expires_idx ON timers (expires);
