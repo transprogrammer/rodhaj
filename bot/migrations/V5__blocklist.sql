f3f23e181f8ae34adab41c275766105c323e0375
No767 <73260931+No767@users.noreply.github.com>
2024-03-26 16:49:22 -0700

    Finalize blocklist schema

--- bot/migrations/V5__blocklist.sql
+++ bot/migrations/V5__blocklist.sql
@@ -7,12 +7,17 @@ CREATE TABLE IF NOT EXISTS blocklist (
     id SERIAL PRIMARY KEY,
     guild_id BIGINT,
     entity_id BIGINT,
-    expires TIMESTAMP,
-    created TIMESTAMP DEFAULT (now() at time zone 'utc'),
-    event TEXT,
+    dispatch_zone TEXT,
+    expires TIMESTAMP WITH TIME ZONE,
+    created TIMESTAMP WITH TIME ZONE DEFAULT (now() at time zone 'utc'),
     UNIQUE (guild_id, entity_id)
 );
 
 CREATE INDEX IF NOT EXISTS blocklist_guild_id_idx ON blocklist (guild_id);
 CREATE INDEX IF NOT EXISTS blocklist_entity_id_idx ON blocklist (entity_id);
+CREATE INDEX IF NOT EXISTS blocklist_dispatch_zone_idx ON blocklist (dispatch_zone);
 CREATE INDEX IF NOT EXISTS blocklist_expires_idx ON blocklist (expires);
+
+-- One feature of the blocklist is to lock tickets.
+-- Thus we need to add an column in the tickets table to account for this
+ALTER TABLE IF EXISTS tickets ADD COLUMN locked BOOLEAN DEFAULT FALSE;
