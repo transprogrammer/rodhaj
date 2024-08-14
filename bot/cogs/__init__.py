100ccfab1e115d79597e7d8951dba4fe70569892
No767 <73260931+No767@users.noreply.github.com>
2024-08-14 09:30:53 -0700

    Bump to v0.3.1 #patch

--- bot/cogs/__init__.py
+++ bot/cogs/__init__.py
@@ -13,4 +13,4 @@ class VersionInfo(NamedTuple):
 
 
 EXTENSIONS = [module.name for module in iter_modules(__path__, f"{__package__}.")]
-VERSION: VersionInfo = VersionInfo(major=0, minor=3, micro=0, releaselevel="final")
+VERSION: VersionInfo = VersionInfo(major=0, minor=3, micro=1, releaselevel="final")
